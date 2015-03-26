c5bf1351c02688288e13640a7fbd50c1d4d9ba8c
ShellMakr <iha2wogu@yahoo.com>
2015-03-26 15:13:03 -0400

    the other stuff

--- renderer.cc
+++ renderer.cc
@@ -135,7 +135,7 @@ void renderFractal(int my_rank, int p, const CameraParams &camera_params, const 
     
     #pragma omp parallel \
     default(none) \
-    shared(ranker, j, farPoint, image, width,to,from,pix_data,renderer_params,camera_params, height, time, my_rank) 
+    shared(ranker, j, farPoint, image, width,to,from,pix_data,renderer_params,camera_params, height, time, my_rank, image_block) 
     {
       for(int i = 0; i < width; i++)
       {
