3756c90975a4521bbcc27b33fddbcfac32ae3871
Graeme Cliffe <graemecliffe@gmail.com>
2015-04-04 19:51:44 -0400

    Included the video making bash file, as well as bash script to clean up the extraneous files

--- MandelBox(GreenBlackRed)/generateParams.sh
+++ MandelBox(GreenBlackRed)/generateParams.sh
@@ -4,6 +4,6 @@ for (( i=0; i<=$1; i++ ))
 do
 	paramFile=params$i.dat
 	sed 's/imageX/image'$i'/' baseParams.dat >$paramFile
-	awk -v i="$i" '{ gsub(/14.0 8.0 10.0/, $1 + 1 * i " 8.0 10.0")};{print}' $paramFile > $paramFile.new
+	awk -v i="$i" '{ gsub(/14.0 8.0 10.0/, $1 - 0.01 * i " 8.0 10.0")};{print}' $paramFile > $paramFile.new
 	mv $paramFile.new $paramFile
 done
---
+++ MandelBox(GreenBlackRed)/makeVideo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+avconv -framerate 30 -i image%d.bmp output.avi
