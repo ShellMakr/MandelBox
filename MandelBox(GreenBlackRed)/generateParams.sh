e8814610b0703703973b64b7b4a50f3d2d1b00fd
Graeme Cliffe <graemecliffe@gmail.com>
2015-04-04 16:52:12 -0400

    Update bash scripts

--- MandelBox(GreenBlackRed)/generateParams.sh
+++ MandelBox(GreenBlackRed)/generateParams.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-i=$1
-for i in $(seq $1 $2)
+for (( i=0; i<=$1; i++ ))
 do
 	paramFile=params$i.dat
 	sed 's/imageX/image'$i'/' baseParams.dat >$paramFile
-	awk -v i="$i" '{ gsub(/14.0 8.0 10.0/, $1 + 0.0001 * i " 8.0 10.0")};{print}' $paramFile > $paramFile.new
+	awk -v i="$i" '{ gsub(/14.0 8.0 10.0/, $1 + 1 * i " 8.0 10.0")};{print}' $paramFile > $paramFile.new
 	mv $paramFile.new $paramFile
 done
