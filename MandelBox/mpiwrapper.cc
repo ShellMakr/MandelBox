5fd7db88d048bfa44960d297598c655c661ddb79
ShellMakr <iha2wogu@yahoo.com>
2015-04-04 13:43:15 -0400

    system call for mandelbox implimented in the wrapper

--- MandelBox/mpiwrapper.cc
+++ MandelBox/mpiwrapper.cc
@@ -1,9 +1,44 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "mpi.h"
 
+void makeParams() {
+	FILE *fp;
+	int ender;
+	static char buf[BUF_SIZE];
 
-int main(int argc, char* arcgv[]) {
 
 
+}
+
+
+int main(int argc, char* argv[]) {
+
+	int my_rank; 			/* rank of process */
+	int p; 					/* number of processes */
+	int tag = 0; 			/* tag for messages */
+	MPI_Status status; 		/* status for receive */	
+
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+	int range = 30 / p;
+
+	for (int i = 0; i < range; ++i)
+	{
+		/* code */
+	}
+
+
+
+	system("./mandelbox params.dat");
+
+
+
+	MPI_Finalize();
+	return 0;
 
 }
