bd59d5aaca9f97e07b14edd487ace82c1cb4ec47
Graeme Cliffe <graemecliffe@gmail.com>
2015-04-04 20:50:32 -0400

    Finished the piecewise version of generate params

--- MandelBox(GreenBlackRed)/generateParams.sh
+++ MandelBox(GreenBlackRed)/generateParams.sh
@@ -2,8 +2,20 @@
 
 for (( i=0; i<=$1; i++ ))
 do
+	echo "Generating file $i"
 	paramFile=params$i.dat
 	sed 's/imageX/image'$i'/' baseParams.dat >$paramFile
-	awk -v i="$i" '{ gsub(/14.0 8.0 10.0/, $1 - 0.01 * i " 8.0 10.0")};{print}' $paramFile > $paramFile.new
-	mv $paramFile.new $paramFile
+	j=$(($i - 615))
+	if [ $i -le 614 ] ;
+	then
+		awk -v i="$i" '{ gsub(/2.4 7 1.3/, "2.4 " 7 - i * 0.0113813 " 1.3")};{print}' $paramFile > $paramFile.new
+		mv $paramFile.new $paramFile
+		awk -v i="$i" '{ gsub(/6 6 12/, "6 6 " 12 - i * 0.009756097)};{print}' $paramFile > $paramFile.new
+		mv $paramFile.new $paramFile
+	else
+		awk -v i="$j" '{ gsub(/6 6 12/, 6*0.99^i" " 6*0.99^i" "6*0.99^i)};{print}' $paramFile > $paramFile.new
+		mv $paramFile.new $paramFile
+		awk '{ gsub(/2.4 7 1.3/, "2.4 " 0.0005 " 1.3")};{print}' $paramFile > $paramFile.new
+		mv $paramFile.new $paramFile
+	fi
 done
