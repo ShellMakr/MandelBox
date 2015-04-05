96f9bbf9b1f08d49c0734c1ee34b087c95e05fa1
ShellMakr <iha2wogu@yahoo.com>
2015-04-05 18:08:31 -0400

    remove mpi stuff from serial

--- MandelBox(GreenBlackRed)/mpiserialwrapper.cc
+++ MandelBox(GreenBlackRed)/mpiserialwrapper.cc
@@ -26,7 +26,6 @@
 #include <string.h>
 #include <iostream>
 #include <fstream>
-#include "mpi.h"
 
 #define BUF_SIZE 1024
 
@@ -34,7 +33,6 @@ static char buf[BUF_SIZE];
 char * filename = "params.dat";
 char * newparams = (char*) calloc(40, sizeof(char));
 int fractal_dec = 0.01137;
-
 int main(int argc, char* argv[]) {
 
 	char * command = (char*) calloc(40, sizeof(char));
@@ -59,3 +57,33 @@ int main(int argc, char* argv[]) {
 	return 0;
 
 }
+
+
+./mpiserialunner 18 22
+./mpiserialunner 27 31
+./mpiserialunner 36 40
+./mpiserialunner 45 49
+./mpiserialunner 72 76
+./mpiserialunner 81 85
+./mpiserialunner 90 94
+./mpiserialunner 99 103
+./mpiserialunner 108 112
+./mpiserialunner 117 121
+./mpiserialunner 126 130
+./mpiserialunner 135 139
+./mpiserialunner 144 148
+./mpiserialunner 153 157
+./mpiserialunner 162 166
+./mpiserialunner 171 175
+./mpiserialunner 180 184
+./mpiserialunner 189 193
+./mpiserialunner 198 202
+./mpiserialunner 207 211
+./mpiserialunner 216 220
+./mpiserialunner 225 230
+./mpiserialunner 234 238
+./mpiserialunner 252 256
+./mpiserialunner 261 265
+./mpiserialunner 270 274
+./mpiserialunner 279 283
+./mpiserialunner 288 292
