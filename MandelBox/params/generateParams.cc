8608de1cad16ac07232cce1edb77444f8e232cde
Graeme Cliffe <graemecliffe@gmail.com>
2015-04-06 18:02:01 -0400

    Included smooth change

--- MandelBox/params/generateParams.cc
+++ MandelBox/params/generateParams.cc
@@ -7,23 +7,35 @@
 #include "renderer.h"
 #include "mandelbox.h"
 
+int FPSForProject = 30;
 
 void getParameters(char *filename, CameraParams *camera_params, RenderParams *renderer_params,
 		   MandelBoxParams *mandelBox_paramsP);
 
+MandelBoxParams mandelBox_params;
+
+float* changeSmooth(int timeInSeconds, float beginValue, float endValue){//Smoothly changes a variable
+	int numIter = FPSForProject*timeInSeconds;
+	float stepSize = (endRange - beginRange)/numIter;
+
+	int i;
+	float varValues[numIter];
+	for (i=0; i<numIter; i++){
+		varValues[i] = stepSize*i + beginRange;
+	}
+	return varValues;
+}
+
 void createParams(const char filename) {
 
 }
-
-
-MandelBoxParams mandelBox_params;
 
 int main(int argc, char* argv[]) {
 
 	CameraParams    camera_params;
   	RenderParams    renderer_params;
   	char * image_name = (char*) calloc(40, sizeof(char));
-	
+
 	printf("%s\n","generating files.." );
 
 	// gets information from the base param file
@@ -32,13 +44,9 @@ int main(int argc, char* argv[]) {
 	int num_of_files = atoi(argv[1]);
 
 	for (int i = 0; i < num_of_files; i++) {
-	
 		sprintf(image_name,"image%d.bmp",i);
 		strcpy(renderer_params.file_name, image_name);
 		printf("%s\n",renderer_params);
-
-
-
 	}
 
 
